d3ff71c854fa2281131b845655a79a0f55202419
MeMyselfandMilo <MeMyselfandMilo@users.noreply.github.com>
2016-04-12 11:43:38 +1000

    Update idCookie(cookieName, inputId) Function.js

--- Scripts/idCookie(cookieName, inputId) Function.js
+++ Scripts/idCookie(cookieName, inputId) Function.js
@@ -2,10 +2,10 @@ function idCookie(cookieName) {
 inputId = 'List'
 var input = document.getElementById(inputId).value
 input = input.replace("\"", "");
-OutputArray = input.split(",")
-var cookieOutput = JSON.stringify(OutputArray)
+Picks = input.split(",")
+var cookieOutput = JSON.stringify(Picks)
 
-createCookie(cookieName, cookieOutput, -1)
+createCookie(cookieName, "", -1)
 createCookie(cookieName, cookieOutput, 30)
  generator()
 }
