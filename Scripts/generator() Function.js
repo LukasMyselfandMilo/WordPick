a7b1dd4cec167ddf2bf2689b43d04f88c187d52f
MeMyselfandMilo <MeMyselfandMilo@users.noreply.github.com>
2016-07-27 11:48:06 +1000

    Update generator() Function.js

--- Scripts/generator() Function.js
+++ Scripts/generator() Function.js
@@ -1,5 +1,5 @@
 function generator() {
-  var Word = Picks[Math.floor(Math.random()*(Picks.length - 1))]
+  var Word = Picks[Math.floor(Math.random()*(Picks.length))]
   Word = Word.replace(" ", "")
   document.getElementById("WordInput").innerHTML = Word
 }
